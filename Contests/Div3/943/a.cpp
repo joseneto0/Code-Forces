7e1d4c00d2d37c8404bc6d22680b12ae109cdee0
José Neto <joseneto.jn1606@gmail.com>
2024-05-09 17:14:03 -0300

    probs de bfs

---
+++ Contests/Div3/943/a.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long int
+#define all(x) x.begin(), x.end()
+#define len(x) (int)x.size()
+#define esac(x) cout << x << "\n"
+
+const int INF = 0x3f3f3f3f;
+const ll LINF = 0x3f3f3f3f3f3f3f3fll;
+const int MAX = 1e5+2;
+
+#ifdef EBUG
+	#include "bug.hpp"
+#else
+	#define bug(x) { ; }
+	#define bug2(x,y) { ; }
+	#define bug3(x,y,z) { ; }
+	#define bugV(V) { ; }
+	#define bugP(V) { ; }
+	#define bugL() { ; }
+#endif
+
+void solve(){
+    int n;
+    cin >> n;
+    esac(n-1);
+}
+
+int main(){
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    int t;
+    cin >> t;
+    while (t--){
+        solve();
+    }
+    return 0;
+}
